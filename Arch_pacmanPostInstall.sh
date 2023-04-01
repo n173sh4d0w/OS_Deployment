489f847ec603542f573d36ae89a04893d5659f55
n173sh4d0w <95049337+n173sh4d0w@users.noreply.github.com>
2023-04-01 06:55:16 +0100

    Update Arch_pacmanPostInstall.sh

--- Arch_pacmanPostInstall.sh
+++ Arch_pacmanPostInstall.sh
@@ -10,6 +10,8 @@ PKGS=(
         'xorg-apps'             # XOrg apps group
         'xorg-xinit'            # XOrg init
         'xf86-video-intel'      # 2D/3D video driver
+        'xserver-xorg-input-synaptics'  #
+        'xf86-input-synaptics'  #touchpad driver
         
         'qtile'                 # Qtile Desktop
         
@@ -29,13 +31,14 @@ PKGS=(
         'blueberry'             # Bluetooth config tool
         'pulseaudio-bluetooth'  # Bluetooth support for PulseAudio
         'bluez-libs' 
-        
+    #Soundcard: Alsa(alsamixer)&Pulseaudio(pavucontrol)
          'alsa-utils'        # Advanced Linux Sound Architecture (ALSA) Components https://alsa.opensrc.org/
          'alsa-plugins'      # ALSA plugins
+         'alsa-firmware'
          'pulseaudio'        # Pulse Audio sound components
          'pulseaudio-alsa'   # ALSA configuration for pulse audio
          'pavucontrol'       # Pulse Audio volume control
-         'volumeicon'        # System tray volume control
+   
          
          'cups'                  # Open source printer drivers
     'cups-pdf'              # PDF support for cups
@@ -47,20 +50,19 @@ PKGS=(
     
     # SYSTEM --------------------------------------------------------------
 
-    'linux-lts'             # Long term support kernel
+    'linux-lts'             # LTS kernel, uname -r 
+    'linux-lts-headers'
 
     # TERMINAL UTILITIES --------------------------------------------------
 
     'bash-completion'       # Tab completion for Bash
-    'bc'                    # Precision calculator language
-    'bleachbit'             # File deletion utility
     'curl'                  # Remote content retrieval
-    'elinks'                # Terminal based web browser
+    'wget'                  # Remote content retrieval
+   # 'elinks'                # Terminal based web browser
     'feh'                   # Terminal-based image viewer/manipulator
-    'file-roller'           # Archive utility
-    'gnome-keyring'         # System password storage
-    'gtop'                  # System monitoring via terminal
-    'gufw'                  # Firewall manager
+    #''         # Archive utility
+    #'gnome-keyring'         # passwordmgr
+    'ufw'                   # Firewall manager
     'hardinfo'              # Hardware info app
     'htop'                  # Process viewer
     'inxi'                  # System information utility
@@ -69,6 +71,7 @@ PKGS=(
     'neofetch'              # Shows system info when you launch terminal
     'ntp'                   # Network Time Protocol to set time via network.
     'numlockx'              # Turns on numlock in X11
+    'screenkey'             #
     'openssh'               # SSH connectivity tools
     'rsync'                 # Remote file sync utility
     'speedtest-cli'         # Internet speed via terminal
@@ -76,12 +79,10 @@ PKGS=(
     'tlp'                   # Advanced laptop power management
     'unrar'                 # RAR compression program
     'unzip'                 # Zip compression program
-    'wget'                  # Remote content retrieval
+    'p7zip-plugins'
     'xfce4-terminal'        # Terminal emulator
     'zenity'                # Display graphical dialog boxes via shell scripts
-    'zip'                   # Zip compression program
-    'zsh'                   # ZSH shell
-    'zsh-completions'       # Tab completion for ZSH
+
 
     # DISK UTILITIES ------------------------------------------------------
 
@@ -94,83 +95,52 @@ PKGS=(
 
     # GENERAL UTILITIES ---------------------------------------------------
 
-    'catfish'               # Filesystem search
-    'conky'                 # System information viewer
-    'nemo'                  # Filesystem browser
+    'fzf'                   # Filesystem search
+    'ranger'                # Filesystem Mgr
     'veracrypt'             # Disc encryption utility
-    'variety'               # Wallpaper changer
-    'xfburn'                # CD burning application
 
     # DEVELOPMENT ---------------------------------------------------------
 
-    'atom'                  # Text editor
-    'apache'                # Apache web server
-    'clang'                 # C Lang compiler
+    'vim'                   # Text editor
+   # 'apache'                # Apache web server
     'cmake'                 # Cross-platform open-source make system
-    'electron'              # Cross-platform development using Javascript
     'git'                   # Version control system
     'gcc'                   # C/C++ compiler
     'glibc'                 # C libraries
-    'mariadb'               # Drop-in replacement for MySQL
-    'meld'                  # File/directory comparison
-    'nodejs'                # Javascript runtime environment
-    'npm'                   # Node package manager
+   # 'nodejs'                # Javascript runtime environment
+   # 'npm'                   # Node package manager
     'php'                   # Web application scripting language
-    'php-apache'            # Apache PHP driver
-    'postfix'               # SMTP mail server
+   #'php-apache'            # Apache PHP driver
+   #'postfix'               # SMTP mail server
     'python'                # Scripting language
-    'qtcreator'             # C++ cross platform IDE
-    'qt5-examples'          # Project demos for Qt
-    'yarn'                  # Dependency management (Hyper needs this)
+    #'yarn'                  # Dependency management (Hyper needs this)
 
     # WEB TOOLS -----------------------------------------------------------
 
-    'chromium'              # Web browser
-    'firefox'               # Web browser
-    'filezilla'             # FTP Client
-    'flashplugin'           # Flash
+#Confirm    'brave-nightly-bin'     # Web browser
+    #'filezilla'             # FTP Client
 
     # COMMUNICATIONS ------------------------------------------------------
 
-    'hexchat'               # Multi format chat
-    'irssi'                 # Terminal based IIRC
 
     # MEDIA ---------------------------------------------------------------
 
-    'lollypop'              # Music player
-    'simplescreenrecorder'  # Record your screen
-    'vlc'                   # Video player
-    'xfce4-screenshooter'   # Screen capture.
+    'ffmpeg'                # Media
+    'flameshot'             # Screen capture.
 
     # GRAPHICS AND DESIGN -------------------------------------------------
 
-    'gcolor2'               # Colorpicker
     'gimp'                  # GNU Image Manipulation Program
-    'inkscape'              # Vector image creation app
-    'imagemagick'           # Command line image manipulation tool
-    'nomacs'                # Image viewer
-    'pngcrush'              # Tools for optimizing PNG images
-    'ristretto'             # Multi image viewer
+    #'imagemagick'           # Command line image manipulation tool
 
     # PRODUCTIVITY --------------------------------------------------------
 
-    'galculator'            # Gnome calculator
     'hunspell'              # Spellcheck libraries
     'hunspell-en'           # English spellcheck library
     'libreoffice-fresh'     # Libre office with extra features
-    'mousepad'              # XFCE simple text editor
-    'xpdf'                  # PDF viewer
-
+    #pdfreader(shuffler/sam)  # PDF viewer
+     #'typora'              #MD Notetaking
     # VIRTUALIZATION ------------------------------------------------------
-
-    'virtualbox'
-    'virtualbox-host-modules-arch'
-    
-    
-    
-    
-    
-    
     
     
     
