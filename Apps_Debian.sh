1ecbe92058d079a2bd969d0b6029ab96471f44eb
n173sh4d0w <95049337+n173sh4d0w@users.noreply.github.com>
2023-04-01 03:29:57 +0100

    Update and rename InstallAppsDebian.sh to Apps_Debian.sh

--- Apps_Debian.sh
+++ Apps_Debian.sh
@@ -18,10 +18,10 @@ sudo apt-get -y autoremove
 
 # 2. CLI SOFTWARE
 
-sudo apt-get install -y build-essential cmake               # DEVELOPMENT TOOLS
-sudo apt-get install -y p7zip p7zip-full unrar-free unzip   # FILE ARCHIVERS
-sudo apt-get install -y htop lshw wget locate               # UTILITIES
-sudo apt-get install -y screen                              # TERMINAL MULTIPLEXER
+sudo apt-get install -y build-essential cmake dkms linux-headers-$(uname -r)         # DEVELOPMENT TOOLS
+sudo apt-get install -y p7zip p7zip-full unrar-free unzip p7zip-plugins unrar tar   # FILE ARCHIVERS
+sudo apt-get install -y htop wget locate ufw              # UTILITIES
+sudo apt-get install -y feh                                 # Photo
 sudo apt-get install -y vim                                 # TEXT EDITORS
 sudo apt-get install -y git                                 # VCS
 sudo apt-get install -y gdisk                               # PARTITION TOOL
@@ -29,7 +29,16 @@ sudo apt-get install -y pdftk                               # PDF MANIPULATION
 sudo apt-get install -y ffmpeg                              # VIDEO MANIPULATION
 sudo apt-get install -y wavemon                             # WIRELESS DEVICE MONITORING
 sudo apt-get install -y default-jre                         # JAVA RUNTIME ENVIRONMENT
-
+sudo apt-get install -y debian-keyring  mousepad 
+sudo apt-get install -y ttf-bitstream-vera  ttf-freefont ttf-mscorefont fonts-noto-mono fonts-droid-fallback fonts-freefont-ttf  fonts-opensymbol hunspell aspell  # icons/fonts(normal, bold, italic) 
+sudo apt-get install -y ranger
+sudo apt-get install -y openssh openssl                     # openssh server
+sudo apt-get install -y
+sudo apt-get install -y
+sudo apt-get install -y
+sudo apt-get install -y
+sudo apt-get install -y
+sudo apt-get install -y
 # sudo apt-get install -y default-jdk                       # JAVA DEVELOPMENT KIT (optional)
 # sudo apt-get install -y tesseract-ocr tesseract-ocr-eng   # OCR (optional)
 
@@ -37,19 +46,18 @@ sudo apt-get install -y default-jre                         # JAVA RUNTIME ENVIR
 # 3. GUI SOFTWARE
 
 sudo apt-get install -y gparted                              # PARTITION TOOL
+sudo apt-get install -y network-manager-openvpn              # NETWORK MANAGER AND OPENVPN
+sudo apt-get install -y pavucontrol                          # VOLUME CONTROL
+sudo apt-get install -y gimp                                 # GRAPHICS EDITORS( inkscape blender)   
+sudo apt-get install -y  libreoffice-calc libreoffice-writer    # OFFICE (optional, not last version)
+#sudo apt-get install -y geany                               # TEXT EDITOR
+#sudo apt-get install -y filezilla                           # FTP/FTPS/SFTP CLIENT
+#sudo apt-get install -y kazam                               # SCREENCAST
 #sudo apt-get install -y gvfs-backends                       # USERSPACE VIRTUAL FILESYSTEM
 #sudo apt-get install -y xarchiver                           # FILE ARCHIVER FRONTEND
 #sudo apt-get install -y network-manager-openvpn-gnome       # NETWORK MANAGER AND OPENVPN FOR GNOME
-sudo apt-get install -y network-manager-openvpn              # NETWORK MANAGER AND OPENVPN
 #sudo apt-get install -y transmission-gtk                    # BITTORRENT CLIENT
 #sudo apt-get install -y galculator                          # SCIENTIFIC CALCULATOR
 #sudo apt-get install -y mpv                                 # VIDEO AND AUDIO PLAYER
-sudo apt-get install -y pavucontrol                          # VOLUME CONTROL
-#sudo apt-get install -y geany                               # TEXT EDITOR
-sudo apt-get install -y gimp                                 # GRAPHICS EDITORS( inkscape blender)   
-#sudo apt-get install -y filezilla                           # FTP/FTPS/SFTP CLIENT
-#sudo apt-get install -y kazam                               # SCREENCAST
-
-# sudo apt-get install -y libreoffice                       # OFFICE (optional, not last version)
 # sudo apt-get install -y texlive-full texmaker             # LATEX (optional, heavy package)
 # sudo apt-get install -y imagemagick                       # IMAGE MANIPULATION PROGRAM (optional)
