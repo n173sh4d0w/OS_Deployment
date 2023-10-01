7d6c47de10a75ad3557634f2f75eaba0ad4ece66
n173sh4d0w <95049337+n173sh4d0w@users.noreply.github.com>
2023-10-01 05:59:43 +0100

    Update WinOS_setup.ps1

--- WinOS_setup.ps1
+++ WinOS_setup.ps1
@@ -1,19 +1,17 @@
-#If the file does not exist, create it.
+# Check if the profile file does not exist, create it
 if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
     try {
-        # Detect Version of Powershell & Create Profile directories if they do not exist.
-        if ($PSVersionTable.PSEdition -eq "Core" ) { 
-            if (!(Test-Path -Path ($env:userprofile + "\Documents\Powershell"))) {
-                New-Item -Path ($env:userprofile + "\Documents\Powershell") -ItemType "directory"
-            }
-        }
-        elseif ($PSVersionTable.PSEdition -eq "Desktop") {
-            if (!(Test-Path -Path ($env:userprofile + "\Documents\WindowsPowerShell"))) {
-                New-Item -Path ($env:userprofile + "\Documents\WindowsPowerShell") -ItemType "directory"
-            }
+        # Determine PowerShell edition and create profile directory if it doesn't exist
+        $profileDir = if ($PSVersionTable.PSEdition -eq "Core") { "Powershell" } else { "WindowsPowerShell" }
+        $profilePath = Join-Path -Path $env:userprofile -ChildPath "Documents\$profileDir"
+        
+        if (!(Test-Path -Path $profilePath)) {
+            New-Item -Path $profilePath -ItemType Directory
         }
 
-        Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
+        # Download and install the profile
+        $profileUrl = "https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1"
+        Invoke-RestMethod -Uri $profileUrl -OutFile $PROFILE
         Write-Host "The profile @ [$PROFILE] has been created."
     }
     catch {
@@ -21,48 +19,42 @@ if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
     }
 }
 # If the file already exists, show the message and do nothing.
- else {
-		 Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
-		 Invoke-RestMethod https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1 -o $PROFILE
-		 Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
- }
+else {
+    Get-Item -Path $PROFILE | Move-Item -Destination oldprofile.ps1
+    $profileUrl = "https://github.com/ChrisTitusTech/powershell-profile/raw/main/Microsoft.PowerShell_profile.ps1"
+    Invoke-RestMethod -Uri $profileUrl -OutFile $PROFILE
+    Write-Host "The profile @ [$PROFILE] has been created and old profile removed."
+}
+
+# Execute the profile
 & $profile
 
-# OMP Install
-#
+# Install OMP
 winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
 
-# Font Install
-# Get all installed font families
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
-$fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
-
-# Check if CaskaydiaCove NF is installed
-if ($fontFamilies -notcontains "CaskaydiaCove NF") {
+# Install Fonts
+if (-not (Get-FontFamily -Name "CaskaydiaCove NF")) {
+    $fontDownloadUrl = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip"
+    $fontPath = Join-Path -Path $PSScriptRoot -ChildPath "CascadiaCode.zip"
+    $fontExtractPath = Join-Path -Path $PSScriptRoot -ChildPath "CascadiaCode"
+    $fontDestination = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Fonts)
     
-    # Download and install CaskaydiaCove NF
-    $webClient = New-Object System.Net.WebClient
-    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
-
-    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
-    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
-        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
-            # Install font
-            $destination.CopyHere($_.FullName, 0x10)
+    Invoke-WebRequest -Uri $fontDownloadUrl -OutFile $fontPath
+    Expand-Archive -Path $fontPath -DestinationPath $fontExtractPath -Force
+    
+    Get-ChildItem -Path $fontExtractPath -Recurse -Filter "*.ttf" | ForEach-Object {
+        $fontFile = $_
+        if (-not (Test-Path (Join-Path -Path $fontDestination -ChildPath $fontFile.Name))) {
+            Copy-Item -Path $fontFile.FullName -Destination $fontDestination
         }
     }
 
-    # Clean up
-    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
-    Remove-Item -Path ".\CascadiaCode.zip" -Force
+    Remove-Item -Path $fontExtractPath -Recurse -Force
+    Remove-Item -Path $fontPath -Force
 }
 
-
-# Choco install
-#
+# Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Terminal Icons Install
-#
+# Install Terminal Icons
 Install-Module -Name Terminal-Icons -Repository PSGallery
