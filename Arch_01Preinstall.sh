a21055de8741c96966fbbfc4cca0a3e9dc14f767
n173sh4d0w <95049337+n173sh4d0w@users.noreply.github.com>
2023-10-01 04:14:27 +0100

    Update Arch_01Preinstall.sh

--- Arch_01Preinstall.sh
+++ Arch_01Preinstall.sh
@@ -1,85 +1,61 @@
 #!/usr/bin/env bash
-#-------------------------------------------------------------------------
-#      _          _    __  __      _   _
-#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
-#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
-#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
-#  Arch Linux Post Install Setup and Config
-#-------------------------------------------------------------------------
+set -e
 
-echo "-------------------------------------------------"
+# Set up mirrors for optimal download - US Only
 echo "Setting up mirrors for optimal download - US Only"
-echo "-------------------------------------------------"
 timedatectl set-ntp true
 pacman-key --init
 pacman-key --populate
-pacman -Syyy
-pacman -S pacman-contrib --noconfirm
+pacman -Sy --noconfirm
+pacman -S --noconfirm pacman-contrib
 mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
+curl -s "https://archlinux.org/mirrorlist/?country=US&protocol=https&use_mirror_status=on" | sed -e '/^#Server/s/^#//' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
 
-echo -e "\nInstalling prereqs...\n$HR"
+# Install prerequisites
+echo -e "\nInstalling prerequisites..."
 pacman -S --noconfirm gptfdisk
 
-echo "-------------------------------------------------"
-echo "-------select your disk to format----------------"
-echo "-------------------------------------------------"
+# Select and format the disk
+echo "Select your disk to format (example /dev/sda):"
 lsblk
-echo "Please enter disk: (example /dev/sda)"
-read DISK
-echo "--------------------------------------"
-echo -e "\nFormatting disk...\n$HR"
-echo "--------------------------------------"
+read -r DISK
+echo -e "\nFormatting disk..."
+sgdisk -Z "$DISK" # zap all on disk
+sgdisk -a 2048 -o "$DISK" # new gpt disk 2048 alignment
 
-# disk prep
-sgdisk -Z ${DISK} # zap all on disk
-sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+# Create partitions
+sgdisk -n 1:0:+1000M "$DISK" # partition 1 (UEFI SYS), default start block, 512MB
+sgdisk -n 2:0:0 "$DISK" # partition 2 (Root), default start, remaining
 
-# create partitions
-sgdisk -n 1:0:+1000M ${DISK} # partition 1 (UEFI SYS), default start block, 512MB
-sgdisk -n 2:0:0     ${DISK} # partition 2 (Root), default start, remaining
+# Set partition types
+sgdisk -t 1:ef00 "$DISK"
+sgdisk -t 2:8300 "$DISK"
 
-# set partition types
-sgdisk -t 1:ef00 ${DISK}
-sgdisk -t 2:8300 ${DISK}
+# Label partitions
+sgdisk -c 1:"UEFISYS" "$DISK"
+sgdisk -c 2:"ROOT" "$DISK"
 
-# label partitions
-sgdisk -c 1:"UEFISYS" ${DISK}
-sgdisk -c 2:"ROOT" ${DISK}
+# Make filesystems
+echo -e "\nCreating Filesystems..."
+mkfs.vfat -F32 -n "UEFISYS" "${DISK}1"
+mkfs.ext4 -L "ROOT" "${DISK}2"
 
-# make filesystems
-echo -e "\nCreating Filesystems...\n$HR"
+# Mount target
+mkdir -p /mnt/boot/efi
+mount -t ext4 "${DISK}2" /mnt
+mkdir -p /mnt/boot
+mount -t vfat "${DISK}1" /mnt/boot/
 
-mkfs.vfat -F32 -n "UEFISYS" "${DISK}p1"
-mkfs.ext4 -L "ROOT" "${DISK}p2"
+# Arch Install on Main Drive
+echo "Arch Install on Main Drive"
+pacstrap /mnt base base-devel linux linux-firmware networkmanager --noconfirm --needed
 
-# mount target
-mkdir /mnt
-mount -t ext4 "${DISK}p2" /mnt
-mkdir /mnt/boot
-mkdir /mnt/boot/efi
-mount -t vfat "${DISK}p1" /mnt/boot/
-
-echo "--------------------------------------"
-echo "-- Arch Install on Main Drive       --"
-echo "--------------------------------------"
-pacstrap /mnt base base-devel --noconfirm --needed
-
-# kernel
-pacstrap /mnt linux linux-firmware --noconfirm --needed
-
-echo "--------------------------------------"
-echo "-- Setup Dependencies               --"
-echo "--------------------------------------"
-
-pacstrap /mnt networkmanager --noconfirm --needed
-
-# fstab
+# Setup Dependencies
+echo "Setup Dependencies"
 genfstab -U /mnt >> /mnt/etc/fstab
 
-echo "--------------------------------------"
-echo "-- Bootloader Systemd Installation  --"
-echo "--------------------------------------"
+# Bootloader Systemd Installation
+echo "Bootloader Systemd Installation"
 bootctl install --esp-path /mnt/boot
 cat <<EOF > /mnt/boot/loader/entries/arch.conf
 title Arch Linux
@@ -90,6 +66,5 @@ EOF
 
 arch-chroot /mnt
 
-echo "--------------------------------------"
-echo "--   SYSTEM READY FOR FIRST BOOT    --"
-echo "--------------------------------------"
+# SYSTEM READY FOR FIRST BOOT
+echo "SYSTEM READY FOR FIRST BOOT"
